4b3460277889b1218ce6f96d58374da458da1342
AjayP123 <30439725+AjayP123@users.noreply.github.com>
2022-10-29 11:57:05 +0100

    5 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'app works!';
+  sports = ['Football', 'Tennis', 'American Footaball', 'Table Tennis'];
 }
-
 
 /*
 Copyright Google LLC. All Rights Reserved.
